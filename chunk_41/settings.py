866ddc5b7d4900affcc2c3b0c9eef7bf6e5a895b
BINAH LOUIS <visteen192@gmail.com>
2022-08-05 18:29:28 +0000

    include all hosts in allowed hosts

--- chunk_41/settings.py
+++ chunk_41/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'chunk_41.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
